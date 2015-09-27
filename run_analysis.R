f76a1df5250bc0c914e96b54df1df30a14b46e8d
Parikshit Lad <parikshitlad@live.com>
2015-09-27 14:56:01 -0500

    minor changes to code and readme files

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,7 @@
 install.packages("reshape2")
+install.packages("data.table")
 library(reshape2)
+library(data.table)
 
 # download, unzip and read file
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
